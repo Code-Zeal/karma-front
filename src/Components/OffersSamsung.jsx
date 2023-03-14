9da7febc4dd3325425d5618136bd962e70b63c9e
Code-Zeal <codeseazeal@gmail.com>
2023-03-14 14:56:01 -0400

    OffersSamsung

--- src/Components/OffersSamsung.jsx
+++ src/Components/OffersSamsung.jsx
@@ -1,15 +1,189 @@
 // @flow
 import React from "react";
-import { Tabs } from "flowbite-react";
+import { Tabs, Carousel } from "flowbite-react";
+import {
+  SparklesIcon,
+  DevicePhoneMobileIcon,
+  TvIcon,
+  ComputerDesktopIcon,
+} from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const OffersSamsung = (props) => {
   return (
-    <section>
-      <Tabs.Group aria-label="Full width tabs" className="w-full">
-        <Tabs.Item title="Profile">Profile content</Tabs.Item>
-        <Tabs.Item title="Dashboard">Dashboard content</Tabs.Item>
-        <Tabs.Item title="Settings">Settings content</Tabs.Item>
-        <Tabs.Item title="Invoice">Invoice content</Tabs.Item>
+    <section className=" ">
+      <div className="w-full h-min bg-white border-t-2 border-slate-800  flex justify-center items-center">
+        <img
+          className="h-[90px]"
+          src="https://www.seekpng.com/png/full/2-21861_samsung-logo-samsung-logo-png-black.png"
+          alt=""
+        />
+      </div>
+      <Tabs.Group aria-label="Tabs with icons" style="underline">
+        <Tabs.Item title="Promociones" icon={SparklesIcon}>
+          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel slideInterval={5000}>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                  alt="..."
+                />
+              </Link>
+            </Carousel>
+          </div>
+        </Tabs.Item>
+        <Tabs.Item active={true} title="Moviles" icon={DevicePhoneMobileIcon}>
+          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel slideInterval={5000}>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                  alt="..."
+                />
+              </Link>
+            </Carousel>
+          </div>
+        </Tabs.Item>
+        <Tabs.Item title="TV & AV" icon={TvIcon}>
+          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel slideInterval={5000}>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                  alt="..."
+                />
+              </Link>
+            </Carousel>
+          </div>
+        </Tabs.Item>
+        <Tabs.Item title="Monitores" icon={ComputerDesktopIcon}>
+          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+            <Carousel slideInterval={5000}>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                  alt="..."
+                />
+              </Link>
+              <Link>
+                <img
+                  className="cursor-pointer w-full"
+                  src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                  alt="..."
+                />
+              </Link>
+            </Carousel>
+          </div>
+        </Tabs.Item>
       </Tabs.Group>
     </section>
   );
