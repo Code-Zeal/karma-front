d82530c6cb709f132dcff9be583e79ab2ae94781
Fabian Torres <fabi.ignacio1998@gmail.com>
2023-04-13 21:21:24 -0400

    intento de hacer que funcione

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -27,15 +27,11 @@ function ProductList() {
       `/product/getProducts?start=${start}&end=${end}`
     );
     const newProducts = response.data.filter(
-      (product) => !productsLoadedIds.includes(product.id)
+      (product) => !products.some((p) => p.id === product.id)
     );
 
     setProducts([...products, ...newProducts]);
     setProductCount(end);
-    setProductsLoadedIds([
-      ...productsLoadedIds,
-      ...newProducts.map((product) => product.id),
-    ]);
   };
 
   const handleScroll = async () => {
@@ -59,10 +55,12 @@ function ProductList() {
 
   return (
     <div>
-      <div className="grid grid-cols-3 gap-4 h-full">
-        {products.slice(0, productCount).map((product, index) => (
-          <Card card={product} key={index} />
-        ))}
+      <div className="grid grid-cols-3 gap-4 h-800 overflow-auto">
+        {products
+          .slice(0, Math.min(productCount, products.length))
+          .map((product, index) => (
+            <Card card={product} key={index} />
+          ))}
       </div>
       <button onClick={handleButtonClick}>Cargar más productos</button>
     </div>
